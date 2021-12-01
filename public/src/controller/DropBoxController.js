c304babbe080d00256b56b9d4a6f00fcf6195894
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-12-01 11:14:57 -0300

    Renomeando arquivo

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -44,6 +44,23 @@ class DropBoxController {
 
   initEvents() {
 
+    this.btnRename.addEventListener('click', e => {
+
+      let li = this.getSelection()[0];
+      let file = JSON.parse(li.dataset.file)
+
+      let name = prompt("Renomear o arquivo:", file.name)
+
+      if (name) {
+        
+        file.name = name;
+
+        this.getFirebaseRef().child(li.dataset.key).set(file)
+
+      }
+
+    })
+
     this.listFilesEl.addEventListener('selectionchange', e =>{
   
       switch (this.getSelection().length) {
@@ -351,7 +368,8 @@ class DropBoxController {
 
     let li = document.createElement('li')
 
-    li.dataset.key = key
+    li.dataset.key = key;
+    li.dataset.file = JSON.stringify(file);
 
     li.innerHTML = `
       ${this.getFileIconView(file)}
